c7831992fab67974ea47479a54dbb8f45273f125
m9n0lo <xthernux@gmail.com>
2023-09-08 08:39:55 -0400

    otro

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: LoginComponent},
+  {path: 'registro/', component: RegistroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
